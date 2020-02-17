12acd59dfa05956b5f7cd6b03533c5e9811396a9
Pritamsuryawanshi <pritam.surya110@gmail.com>
2020-02-17 19:21:22 +0530

    ADDED: REGEX pattern and Test cases for valid phone number

--- src/main/java/com/userregistration/UserRegistrationMain.java
+++ src/main/java/com/userregistration/UserRegistrationMain.java
@@ -4,6 +4,7 @@ public class UserRegistrationMain {
     private static final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}";
     private static final String LAST_NAME_PATTERN = FIRST_NAME_PATTERN;
     private static final String EMAIL_ID_PATTERN = "^[a-zA-Z]{3,}([.-]?[a-zA-Z0-9]+)?[@]{1}[0-9a-zA-Z]+[.]{1}[a-zA-Z]{2,3}([.]?[a-zA-Z]+)?$";
+    private static final String PHONE_NUMBER_PATTERN = "^[0-9]{2}[ ][0-9]{10}$";
 
     public static void main(String[] args) {
         System.out.println("Welcome to User Registrations");
@@ -22,5 +23,10 @@ public class UserRegistrationMain {
     // Function to check for valid email Id
     public boolean checkForValidEmailId(String emailId) {
         return emailId.matches(EMAIL_ID_PATTERN);
+    }
+
+    public boolean checkForValidPhoneNumber(String phoneNumber) {
+        if (phoneNumber.matches(PHONE_NUMBER_PATTERN)) return true;
+        else return false;
     }
 }
--- src/test/java/UserRegistration.java
+++ src/test/java/UserRegistration.java
@@ -3,10 +3,10 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class UserRegistration {
-    //Object Created to access the main class
+    // Object Created to access the main class
     UserRegistrationMain userRegistrationObject = new UserRegistrationMain();
 
-    //TESTING FOR FIRST NAME
+    // TESTING FOR VALID FIRST NAME
     @Test
     public void givenFirstName_WhenProper_ShouldReturnTrue() {
         boolean validFirstNameResult1 = userRegistrationObject.checkForValidFirstName("Pritam");
@@ -25,7 +25,7 @@ public class UserRegistration {
         Assert.assertFalse(validFirstNameResult3);
     }
 
-    //TESTING FOR LAST NAME
+    // TESTING FOR VALID LAST NAME
     @Test
     public void givenLastName_WhenProper_ShouldReturnTrue() {
         boolean validLastNameResult1 = userRegistrationObject.checkForValidLastName("Suryawanshi");
@@ -44,10 +44,11 @@ public class UserRegistration {
         Assert.assertFalse(validLastNameResult3);
     }
 
+    // TESTING FOR VALID EMAIL ID
     @Test
     public void givenEmailId_WhenProper_ShouldReturnTrue() {
-    boolean validEmailIdResult1 = userRegistrationObject.checkForValidEmailId("pritam.surya110@gmail.com");
-    Assert.assertTrue(validEmailIdResult1);
+        boolean validEmailIdResult1 = userRegistrationObject.checkForValidEmailId("pritam.surya110@gmail.com");
+        Assert.assertTrue(validEmailIdResult1);
     }
 
     @Test
@@ -61,5 +62,20 @@ public class UserRegistration {
         boolean validEmailIdResult3 = userRegistrationObject.checkForValidEmailId("abc.@gmail.com");
         Assert.assertFalse(validEmailIdResult3);
     }
+
+    // TESTING FOR VALID PHONE NUMBER
+    @Test
+    public void givenPhoneNumber_WhenProper_ShouldReturnTrue() {
+        boolean validPhoneNumberResult1 = userRegistrationObject.checkForValidPhoneNumber("91 8169842509");
+        Assert.assertTrue(validPhoneNumberResult1);
+    }
+
+    @Test
+    public void givenPhoneNumber_WhenImproper_ShouldReturnFalse() {
+        boolean validPhoneNumberResult2 = userRegistrationObject.checkForValidPhoneNumber("918169842509");
+        Assert.assertFalse(validPhoneNumberResult2);
+    }
+
+
 }
 
